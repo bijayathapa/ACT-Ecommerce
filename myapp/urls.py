25f745dc31857b5914b0967b38ea65634f422f19
Bijaya <t.sabijaya@gmail.com>
2021-09-03 12:36:00 +0545

    Cart and checkout backend

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
+    path('',views.store, name="store"),
     path('store/',views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import * 
 # Create your views here.
 
 def index(request):
@@ -7,13 +7,36 @@ def index(request):
     return render(request, 'index.html', context)
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
     return render(request, 'store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitems_set.all()
+    else:
+        items = []
+        order = {'get_cart_items':0, 'get_cart_total': 0}
+
+    context= {'items': items,
+        'order':order
+    }
     return render(request, 'cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitems_set.all()
+    else:
+        items = []
+        order = {'get_cart_items':0, 'get_cart_total': 0}
+
+    context= {'items': items,
+        'order':order
+    }
     return render(request, 'checkout.html', context)
